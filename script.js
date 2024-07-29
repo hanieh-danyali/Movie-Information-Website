8a0ab21a3cd05ef27c90af69d635a4bb1dc38874
fatemeh <f2234204@gmail.com>
2024-07-29 11:59:39 -0700

    change styles

--- script.js
+++ script.js
@@ -3,40 +3,35 @@ const img_actor1 = document.querySelector('.img_actor1');
 const img_actor2 = document.querySelector('.img_actor2');
 const img_actor3 = document.querySelector('.img_actor3');
 const img_actor4 = document.querySelector('.img_actor4');
+const name_actor = document.querySelector('.name_actor');
+const about_actor = document.querySelector('.about_actor');
+const image_actor = document.querySelector('.image_actor');
 
-const info__artists1 = document.querySelector('.info__artists1');
 
 img_actor1.addEventListener('click',function(){
-    info__artists1.classList.add('move1');
-    info__artists1.classList.remove('move2');
-    info__artists1.classList.remove('move3');
-    info__artists1.classList.remove('move4');
-})
-
+    name_actor.textContent ='Jenna Ortega';
+    about_actor.textContent = `American actress and voice actress Jenna Ortega was born on September 27, 2002 in Coachella Valley, California. Besides acting, she is also interested in writing,
+     her book was published in January 2022 with the name (It's All Love).
+        Jenna Ortega has a history of acting and voicing in films and series such as Wednesday, Scream 5, Yes Day, Princess Elena, Jurassic World: Camp Cretaceous, Saving Flora, You and Iron Man 3 in her artistic career.`;
+    image_actor.src = 'img/photo12814841522.jpg';
+});
 
 img_actor2.addEventListener('click',function(){
-    info__artists1.textContent='fatemeh abbasi';
-    info__artists1.classList.add('move2');
-    info__artists1.classList.remove('move1');
-    info__artists1.classList.remove('move3');
-    info__artists1.classList.remove('move4');
-    
+    name_actor.textContent ='Hunter Doohan';
+    about_actor.textContent = 'Hunter Doohan Hunter Doohan Hunter Doohan Hunter Doohan Hunter Doohan';
+    image_actor.src = 'img/photo12815550593.jpg';
 });
 
 img_actor3.addEventListener('click',function(){
-    // info__artists1.classList.remove('move1');
-    info__artists1.textContent='hesaaaam';
-    info__artists1.classList.add('move3');
-    info__artists1.classList.remove('move1');
-    info__artists1.classList.remove('move2');
-    info__artists1.classList.remove('move4');
+    name_actor.textContent ='Emma Myers';
+    about_actor.textContent = 'Emma Myers Emma Myers Emma Myers Emma Myers Emma Myers Emma Myers Emma Myers Emma Myers';
+    image_actor.src = 'img/photo12810731170.jpg';
 });
 
 img_actor4.addEventListener('click',function(){
-    // info__artists1.classList.remove('move1');
-    info__artists1.textContent='amir mohammad';
-    info__artists1.classList.add('move4');
-    info__artists1.classList.remove('move3');
-    info__artists1.classList.remove('move2');
-    info__artists1.classList.remove('move1');
+    name_actor.textContent ='Percy Hynes White';
+    about_actor.textContent = `American actress and voice actress Jenna Ortega was born on September 27, 2002 in Coachella Valley, California. Besides acting, she is also interested in writing,
+     her book was published in January 2022 with the name (It's All Love).
+        Jenna Ortega has a history of acting and voicing in films and series such as Wednesday, Scream 5, Yes Day, Princess Elena, Jurassic World: Camp Cretaceous, Saving Flora, You and Iron Man 3 in her artistic career.`;
+    image_actor.src = 'img/photo12810941916.jpg';
 });
