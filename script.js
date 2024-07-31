7af6ced3a79a97bea80d67d057d55ef3469ba51d
fatemeh <f2234204@gmail.com>
2024-07-31 08:35:22 -0700

    compelete styles

--- script.js
+++ script.js
@@ -26,14 +26,14 @@ Then he played in other series such as "Schooled" in t. In 2022, at the age of 2
 
 img_actor3.addEventListener('click',function(){
     name_actor.textContent ='Emma Myers';
-    about_actor.textContent = `Emma Myers (English: Emma Myers; born April 2, 2002) is an American actress.when he appeared in the movie Bisheh. He became famous by playing the role of Inde Sinclair in the series Wednesday (2022). She has a sister named Isabel Myers. Emma Myers was born on April 2, 2002 in Orlando, Florida. He was home schooled and never had a traditional school experience. He began acting professionally at the age of 16. started. Myers appeared in the films Southern Gospel (2020). She rose to fame in the 2022 TV series Wednesday playing the role of End Sinclair alongside co-star Jenna Ortega, who played Wednesday Addams.`;
+    about_actor.textContent = `Emma Myers (English: Emma Myers; born April 2, 2002) is an American actress.when he appeared in the movie Bisheh. He became famous by playing the role of Inde Sinclair in the series Wednesday (2022). She has a sister named Isabel Myers. Emma Myers was born on April 2, 2002 in Orlando. He began acting professionally at the age of 16. started. Myers appeared in the films Southern Gospel (2020). She rose to fame in the 2022 TV series Wednesday playing the role of End Sinclair alongside co-star Jenna Ortega, who played Wednesday Addams.`;
     image_actor.src = 'img/photo_emma.png';
-    image_actor.classList.remove('hidden');
+    image_actor.classList.add('hidden');
 });
 
 img_actor4.addEventListener('click',function(){
     name_actor.textContent ='Percy Hynes White';
     about_actor.textContent = `Percy Haynes White was born on October 8, 2001 and is a Canadian actor. He is known for his roles in films such as A Christmas Horror Story (2015), The Edge of Winter (2016) and for his role in the television series The Middle (2015) and for his starring role as Andy Strucker in The Talented (2017). In 2022, he starred as Xavier Thorpe in the comedy-horror Wednesday. Hines White is the son of writer, actor and director Joel Thomas Hines and actor and writer Sherry White. Percy studied performing arts in his hometown of St. John's for two years.`;
-    image_actor.src = 'img/photo_percy.png';
-    image_actor.classList.add('hidden');
+    image_actor.src = 'img/photo12850770410_prev_ui.png';
+    image_actor.classList.remove('hidden');
 });
